3ed756420b70dd3053606836824ce4b7036264d3
Renan Mello <renandiasmello182@gmail.com>
2019-10-10 15:45:57 -0300

    Insert try catch to validated connection.

--- connection.php
+++ connection.php
@@ -1,5 +1,8 @@
 <?php
 
-$pdo = new PDO('mysql:dbname=project_hogarth;host=localhost', 'root', '', [PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8']);
-
-return $pdo;
+    try {
+        $pdo = new PDO('mysql:dbname=project_hogarth;host=localhost', 'root', '', [PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8']);
+        return $pdo;
+    } catch (Exception $e) {
+        die("Error to try connect " . $e->getMessage());
+    }
